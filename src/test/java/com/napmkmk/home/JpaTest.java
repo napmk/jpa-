26b3a3761e37da010ee6f14be270f7df0947ba01
ici <ici@DESKTOP-J9CT40H>
2022-12-02 17:29:02 +0900

    test

--- src/test/java/com/napmkmk/home/JpaTest.java
+++ src/test/java/com/napmkmk/home/JpaTest.java
@@ -21,21 +21,21 @@ public class JpaTest {
 	@Autowired
 	MemberRepository memberRepository;
 	
-	@Test
-	@DisplayName("이름 검색 테스트")
-	public void searchName() {
-		List<MemberDto> dtos = memberRepository.findByNameContaining("길");
-		
-		for(MemberDto memberDto : dtos) {
-			System.out.println(memberDto.toString());
-		};
-		
-		
-	}
-	
+//	@Test
+//	@DisplayName("이름 검색 테스트")
+//	public void searchName() {
+//		List<MemberDto> dtos = memberRepository.findByNameContaining("길");
+//		
+//		for(MemberDto memberDto : dtos) {
+//			System.out.println(memberDto.toString());
+//		};
+//		
+//		
+//	}
+//	
 	
 //	@Test
-//	@DisplayName("회원 삭제 테스트")
+//	@DisplayName("회원 삭제 테스트") // DELETE
 //	public void deleteMember() {
 //		Long hak = (long) 1;
 //		
@@ -59,7 +59,7 @@ public class JpaTest {
 //	}
 
 //	@Test
-//	@DisplayName("회원 삭제 테스트")
+//	@DisplayName("회원 삭제 테스트") // DELETE
 //	public void deleteMember() {
 //		Long hak = (long) 2;
 //		
@@ -90,15 +90,39 @@ public class JpaTest {
 //		};
 //			
 //	}
+	
+	
 //	@Test
-//	@DisplayName("특정 회원 삭제")
+//	@DisplayName("특정 회원 삭제") // DELETE
 //	public void deleteMember() {
-//		memberRepository.deleteAllByName("커피");
+//		memberRepository.deleteById(3L); //3번 Long 형을 지워라
 //		List<MemberDto> memberDtos = memberRepository.findAll();
 //		
 //		for(MemberDto Dto : memberDtos) {
 //			System.out.println(Dto.toString());
 //		};
 //	}
-//	
+	
+	@Test
+	@DisplayName("회원 정보 수정 테스트")  //UPDATE
+	public void modifyMember() {
+		
+	
+		Optional<MemberDto> memberDto = memberRepository.findById(3L);
+		assertTrue(memberDto.isPresent());// 존재확인
+		
+		MemberDto dto = memberDto.get();
+		dto.setName("이순신"); //이름수정
+		
+		memberRepository.save(dto);
+		
+		List<MemberDto> memberDtos = memberRepository.findAll();
+		
+		
+		for(MemberDto Dto : memberDtos) {
+			System.out.println(Dto.toString());
+		};
+		
+	}
+	
 }
